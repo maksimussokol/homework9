76d9bf08fcf258d8b2141c1c03ff2198f9b7b965
maksimussokol <maksimus@2074.ru>
2022-10-20 21:12:11 +0500

    Vse pashet

---
+++ ex66/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int first = n;
+int last = m;
+
+if (n > m)
+{
+    first = m;
+    last = n;
+}
+
+int FindSum(int poslednee)
+{
+    if (poslednee == 0) return 0;
+    else return poslednee + FindSum(poslednee - 1);
+}
+Console.WriteLine($" summa ot pervogo do poslednego = {FindSum(last) - FindSum(first - 1)}");
---
+++ ex68/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int Aker(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return Aker(m - 1, 1);
+    }
+    return Aker(m - 1, Aker(m, n - 1));
+}
+
+Console.WriteLine(Aker(m, n));
